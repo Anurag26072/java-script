c2ded25b3d8fa0287485408da2a0df83e50faabc
Anurag26072 <anuragmishra26july2007@gmail.com>
2026-02-09 10:14:51 +0530

    Delete 2 directory

--- 2/script (1).js
+++
@@ -1,7 +0,0 @@
-const submitButton = document.querySelector("#submitBtn");
-
-const toggleButton = document.querySelector("#toggleBtn");
-
-toggleButton.addEventListener("click", function () {
-    submitButton.classList.toggle("btn");
-});
